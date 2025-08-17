46707b91a22c65199edb4d46b6c2774e85782360
unknown <landenlabs@gmail.com>
2025-08-17 10:09:57 -0400

    update visual studio path from f: to g:

--- install-llTimeMe.bat
+++ install-llTimeMe.bat
@@ -11,7 +11,7 @@ if not exist "%dstdir%" (
 echo "msbuild=%msbuild%"
 if not exist "%msbuild%" (
 echo Fall back msbuild not found at "%msbuild%"
-set msbuild=F:\opt\VisualStudio\2022\Preview\MSBuild\Current\Bin\MSBuild.exe
+set msbuild=G:\opt\VisualStudio\2022\Preview\MSBuild\Current\Bin\MSBuild.exe
 )
 echo "Msbuild=%msbuild%"
 
